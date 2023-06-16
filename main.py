a3f3a18309564f182955413c590233ec33df676c
Gabriel Andrade <gabriel.santana@gmail.com>
2023-06-16 01:27:04 -0300

    update code

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask,request,jsonify
 import os
+from joblib import load
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    if 'query' not in request.args:
+        return jsonify({"prediction":None,"message":"sendme a text"})
 
+    query = request.args.get("query")
+    model = load("model.joblib")
+    labels = ['Linguagens e Frameworks','Plataformas','Ferramentas']
+
+    predict = model.predict([query])
+    prediction = labels[predict[0]]
+
+    return jsonify({"prediction":prediction})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
